65baa63433a6eadd3df7d7a6a9770838c3ace90a
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2023-07-22 11:16:05 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
+import Modal from './Modal';
+import Spinner from './Spinner';
+import '../styles.css';
+
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleFormSubmit = query => {
+    setSearchQuery(query);
+    setImages([]);
+    setPage(1);
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  useEffect(() => {
+    if (!searchQuery) return;
+
+    setIsLoading(true);
+
+    const API_KEY = '37184113-cc7f1841943926b48c61b8d8a';
+    const URL = `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        setImages(prevImages => [...prevImages, ...data.hits]);
+        setIsLoading(false);
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      });
+  }, [searchQuery, page]);
+
+  const handleImageClick = image => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {isLoading && <Spinner />}
+      {!!images.length && <Button onClick={handleLoadMore} />}
+      {selectedImage && <Modal image={selectedImage} onClose={handleCloseModal} />}
     </div>
   );
 };
+
+export default App;
